45a3cb19bddf34f10bdbbaf4a97902950739a8d4
Anatolii Sakhnik <sakhnik@gmail.com>
2017-11-18 22:45:33 +0200

    Avoid string duplication

--- 5-Burrows/src/CircularSuffixArray.java
+++ 5-Burrows/src/CircularSuffixArray.java
@@ -22,19 +22,23 @@ public class CircularSuffixArray {
         for (int i = 0; i < idx.length; ++i)
             idx[i] = i;
 
-        String buffer = s + s;
-
         class Cmp implements Comparator<Integer>
         {            
             @Override
             public int compare(Integer i1, Integer i2)
             {
-                for (int i = i1, n = i + idx.length, j = i2;
-                        i < n; ++i, ++j) {
-                    char c1 = buffer.charAt(i);
-                    char c2 = buffer.charAt(j);
-                    if (c1 == c2)
+                int a = i1;
+                int b = i2;
+                for (int i = 0; i < s.length(); ++i) {
+                    char c1 = s.charAt(a);
+                    char c2 = s.charAt(b);
+                    if (c1 == c2) {
+                        if (++a >= s.length())
+                            a = 0;
+                        if (++b >= s.length())
+                            b = 0;
                         continue;
+                    }
                     return c1 < c2 ? -1 : 1;
                 }
 
